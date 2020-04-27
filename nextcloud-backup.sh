a59e3bbb433e4a8e20743d5da078421609e8e395
Chris Kay <karasoulis@protonmail.com>
2020-04-27 10:33:48 +0300

    Update nextcloud-backup.sh

--- nextcloud-backup.sh
+++ nextcloud-backup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 #Start
-echo "Backing up your Nextcloud..."
+echo "Starting your Nextcloud backup..."
+
+#Clean up
+echo "Cleaning up old backups"
+rm -rf /var/snap/nextcloud/common/backups/*
+echo "All clear!"
 
 #Export
 echo "Starting the export"
 nextcloud.export
 echo "Export complete"
 
-#Compression
-echo "Compressing your backup..."
-tar -czvf ~/backups/"$(date '+%Y-%m-%d').tar.gz" /var/snap/nextcloud/common/backups/* 
-echo "Compression complete at your home/backups folder"
-
-#Clean up
-echo "Cleaning up"
-rm -rf /var/snap/nextcloud/common/backups/*
-echo "All clear!"
-
 #Sync
 echo "Syncing to HDD1000GB"
 rsync -aHv ~/backups/* ~/HDD1000GB/backups
-echo "Syncing complete"
+echo "Sync complete"
+
+#Compression
+#echo "Compressing your backup..."
+#tar -czvf ~/HDD1000GB/backups/"$(date '+%Y-%m-%d').tar.gz" ~/HDD1000GB/backups/* 
+#echo "Compression complete at your home/backups folder"
 
 #End
 echo "Nextcloud backup completed successfully!" 
