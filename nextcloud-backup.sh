1ace4151bd8e02d60eb61e3173b827d17dad1f1e
Chris Kay <karasoulis@protonmail.com>
2020-05-06 08:29:22 +0300

    Update nextcloud-backup.sh

--- nextcloud-backup.sh
+++ nextcloud-backup.sh
@@ -15,14 +15,14 @@ echo "Export complete"
 
 #Sync
 echo "Syncing to HDD1000GB"
-rsync -aHv ~/backups/* ~/HDD1000GB/backups
+rsync -aHv /var/snap/nextcloud/common/backups/* ~/HDD1000GB/backups
 echo "Sync complete"
 
 #Compression
-#echo "Compressing your backup..."
-#tar -czvf ~/HDD1000GB/backups/"$(date '+%Y-%m-%d').tar.gz" ~/HDD1000GB/backups/* 
-#echo "Compression complete at your home/backups folder"
+#echo "Compressing your backup"
+#tar -czvf ~/HDD1000GB/backups/"$(date '+%Y-%m-%d').tar.gz" /var/snap/nextcloud/common/backups/*
+#echo "Compression complete."
 
 #End
 echo "Nextcloud backup completed successfully!" 
-echo "You'll find the new backup on your HDD1000GB in the backups folder."
+echo "You'll find the new backup on your HDD1000GB drive in the backups folder."
